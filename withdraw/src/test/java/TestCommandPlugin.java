48895993c75d53f14a9346f6ba7dbbb5d38795c4
surya9109 <surya9109@gmail.com>
2021-09-14 18:19:57 +0530

    first

---
+++ withdraw/src/test/java/TestCommandPlugin.java
@@ -0,0 +1,20 @@
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import schemacrawler.tools.executable.CommandRegistry;
+
+public class TestCommandPlugin
+{
+
+  @Test
+  public void testCommandPlugin()
+    throws Exception
+  {
+    final CommandRegistry registry = new CommandRegistry();
+    assertThat(registry.isCommandSupported("additional"), is(true));
+  }
+
+}
